49e04ee71fcf4a063f2c1c8f3614fb6e8a1a78ff
wellingp <patrice.wellington@gmail.com>
2022-06-02 16:06:44 -0400

    Completed basic JS functionality, not quite what I want yet.

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,6 +1,20 @@
 
-document.getElementsByClassName(".davy").forEach(function(element) {
-	element.addEventListener('click', function() {
-		alert('working?')
-	});
-});
+let davy = document.getElementsByClassName("davy");
+let mike = document.getElementsByClassName("mike");
+let micky = document.getElementsByClassName("micky");
+let peter = document.getElementsByClassName("peter");
+
+let davyBox = document.querySelector('td.davy');
+let mikeBox = document.querySelector('td.mike');
+let mickyBox = document.querySelector('td.micky');
+let peterBox = document.querySelector('td.peter');
+
+function addDavyListener(){
+	for(var i = 0; i < davy.length; i++) {
+		davy[i].addEventListener('click', function changePicture(){
+			this.classList.toggle('davy-picture');
+		});
+	}
+}
+
+addDavyListener();

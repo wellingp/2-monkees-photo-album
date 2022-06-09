0183f4dd1c46c5be5c08b10c374f4c2244bbf1df
wellingp <patrice.wellington@gmail.com>
2022-06-09 15:44:34 -0400

    Improved graphics. Fiddling with JS.

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -7,7 +7,10 @@ let peter = document.getElementsByClassName("peter");
 let h1 = document.querySelector('h1');
 let h2 = document.querySelector('h2');
 
-function addListener(whichMonkee, whichClass, whichMessage){
+var otherMonkees = document.querySelectorAll('td');
+var otherMonkeesArray = Array.from(otherMonkees);
+
+function addListener(whichMonkee, whichClass, whichMessage, reset){
 	for(var i = 0 ; i < 4 ; i++) {
 		whichMonkee[i].addEventListener('click', function changePicture(){
 			
@@ -20,14 +23,32 @@ function addListener(whichMonkee, whichClass, whichMessage){
 				h2.style.visibility = 'hidden';
 			}
 
+			//Error: not defined for the below.
+
+			var resetMonkees = otherMonkeesArray.filter(monkeeFilter);
+
+			function monkeeFilter(monkee){
+				return monkee.classList !== whichMonkee;
+			}
+
+			
+
+			
+
+			// if (otherMonkees[key] == whichMonkee){
+			// 	for(i = 0 ; i < 12 ; i++){
+			// 		otherMonkees[i].classList.toggle(reset);
+			// 	}
+			// }
+
 		});
 	}
 }
 
 // Find a way to reset squares.
 
-addListener(davy, 'davy-picture', 'How fab, it\'s Davy!');
-addListener(mike, 'mike-picture', 'Groovy! Mike is where it\'s at!');
-addListener(micky, 'micky-picture', 'Cool! You dig Micky!');
-addListener(peter, 'peter-picture', 'Man, that\'s Peter!');
+addListener(davy, 'davy-picture', 'How fab, it\'s Davy!', davy);
+addListener(mike, 'mike-picture', 'Groovy! Mike is where it\'s at!', mike);
+addListener(micky, 'micky-picture', 'Cool! You dig Micky!', micky);
+addListener(peter, 'peter-picture', 'Man, that\'s Peter!', peter);
 
